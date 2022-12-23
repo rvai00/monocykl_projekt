f71d670d66f0720f5a181c74164c2d300652bdeb
arwondianlu <120572220+arwondianlu@users.noreply.github.com>
2022-12-23 12:02:11 +0100

    moja zmiana

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
   const name = "Oli Wasiuk" 
   const name_hanna = "Hanna Hryshkevich"
   const name_nina = "Nina Niewińska"
+  const name_agata = "Agata Sawicka"
     return (
     <div className="App">
     <div> {name} </div>
     <div>{name_hanna}</div>
     <div>{name_nina}</div>
     <div>Dawid Pawlak</div>
-    <div>Agata Sawicka</div>
+    <div>{name_agata}</div>
     </div>
   );
 }
