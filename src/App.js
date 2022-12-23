308e45b0cb33eeb1a4e82d2de639b87ddc376eaf
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2022-12-23 12:03:56 +0100

    Dodanie imienia

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
   const name_hanna = "Hanna Hryshkevich"
   const name_nina = "Nina Niewińska"
   const name_agata = "Agata Sawicka"
+  const name_dawid = "Dawid Pawlak"
     return (
     <div className="App">
     <div> {name} </div>
     <div>{name_hanna}</div>
     <div>{name_nina}</div>
-    <div>Dawid Pawlak</div>
+    <div>{name_dawid}</div>
     <div>{name_agata}</div>
     </div>
   );
