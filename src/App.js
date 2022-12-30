2064a2b2de4c8ce1d9f7fd7021b49f63dd770c65
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2022-12-30 13:25:00 +0100

    Obiekty

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ const updateNameHanna = (name_hanna) => {
   return updatedNameHanna
 }
 
+const generateDawid = (name, surname) => {
+    return {name: name, surname: surname};
+}
+
 function App() {
   const name = "Oli Wasiuk" 
   const name_hanna = "Hanna Hryshkevich"
   const name_nina = "Nina Niewińska"
   const name_agata = "Agata Sawicka"
-  const name_dawid = "Dawid Pawlak"
+  const dawid = generateDawid("Dawid", "Pawlak");
     return (
     <div className="App">
     <div> {name} </div>
     <div>{updateNameHanna(name_hanna)}</div>
     <div>{name_nina}</div>
-    <div>{name_dawid}</div>
+    <div>{dawid.name + " " + dawid.surname}</div>
     <div>{name_agata}</div>
     </div>
   );
