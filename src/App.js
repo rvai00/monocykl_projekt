b554ed92dfc620fbdb046e2a8f291093056f6046
rvai00 <120733021+rvai00@users.noreply.github.com>
2023-01-12 21:11:58 +0100

    <zmiana>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 const updateNameHanna = (name_hanna) => {
   const updatedNameHanna = name_hanna.toUpperCase()
   return updatedNameHanna
@@ -22,14 +23,23 @@ const updateNameNina = (name_nina) => {
   return updatedNameNina
 }
 
+const updateName = (name, surname) => {
+  const newName = name.slice (2,8)
+  const newSurname = surname.slice (4,5)
+  return {name: newName,surname: newSurname}
+}
+
+
 function App() {
-  const name = "Oli Wasiuk" 
+  const name = "Oli" 
+  const surname = "Wasiuk"
   const name_hanna = "Hanna Hryshkevich"
   const name_nina = "Nina Niewińska"
   const name_agata = "Agata"
   const surname_agata = "Sawicka"
   const agata = updateNameAgata(name_agata, surname_agata);
   const dawid = generateDawid("Dawid Pawlak");
+  const oli = updateName(name, surname)
     return (
     <div className="App">
     <div> {name} </div>
@@ -37,6 +47,7 @@ function App() {
     <div>{updateNameNina(name_nina)}</div>
     <div>{dawid.name + " " + dawid.surname}</div>
     <div>{agata.name_agata + " " + agata.surname_agata}</div>
+    <div>{oli.name + " " + oli.surname}</div>
     </div>
   );
 }
