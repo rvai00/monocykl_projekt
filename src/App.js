31db8aee068cea78d804a0183af3e6b3c43a7273
Oli <120733021+rvai00@users.noreply.github.com>
2023-01-12 20:53:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,14 +22,22 @@ const updateNameNina = (name_nina) => {
   return updatedNameNina
 }
 
+const updateName = (name, surname) => {
+  const newName = name.slice (2,8)
+  const newSurname = surname.slice (4,5)
+  return {name: newName,surname: newSurname}
+}
+
 function App() {
-  const name = "Oli Wasiuk" 
+  const name = "Oli" 
+  const surname = "Wasiuk"
   const name_hanna = "Hanna Hryshkevich"
   const name_nina = "Nina Niewińska"
   const name_agata = "Agata"
   const surname_agata = "Sawicka"
   const agata = updateNameAgata(name_agata, surname_agata);
   const dawid = generateDawid("Dawid Pawlak");
+  const oli = updateName(name, surname);
     return (
     <div className="App">
     <div> {name} </div>
@@ -37,6 +45,7 @@ function App() {
     <div>{updateNameNina(name_nina)}</div>
     <div>{dawid.name + " " + dawid.surname}</div>
     <div>{agata.name_agata + " " + agata.surname_agata}</div>
+    <div>{oli.name + " " + oli.surname}</div>
     </div>
   );
 }
