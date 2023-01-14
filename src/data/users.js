7bc2e51465a7eb227e519ff03d8bbb320a33d103
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2023-01-15 00:41:58 +0100

    Dodanie swych danych

--- src/data/users.js
+++ src/data/users.js
@@ -2,5 +2,6 @@ export const users = [
     {name:"Nina", surname: "Niewińska"},
     {name:"Agata", surname:"Sawicka"},
     {name:"Oli", surname:"Wasiuk"},
+    {name:"Dawid", surname:"Pawlak"},
 
 ]
