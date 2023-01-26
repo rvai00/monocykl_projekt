b5b546f0a34a66a42692d02f2ff09e93ad27e437
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2023-01-26 20:13:04 +0100

    Badges preparation

--- src/data/users.js
+++ src/data/users.js
@@ -2,7 +2,7 @@ export const users = [
     {name:"Nina", surname: "Niewińska"},
     {name:"Agata", surname:"Sawicka"},
     {name:"Oli", surname:"Wasiuk"},
-    {name:"Dawid", surname:"Pawlak"},
+    {name:"Dawid", surname:"Pawlak", badges: []},
     {name: "Hanna", surname: "Hryshkevich"},
 
 ]
