6752201b9f140990c1cb5f0701068492f9e87a83
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2023-01-27 12:38:23 +0100

    Dodanie odznak

--- src/data/users.js
+++ src/data/users.js
@@ -2,7 +2,7 @@ export const users = [
     {name:"Nina", surname: "Niewińska"},
     {name:"Agata", surname:"Sawicka", badges: ["łączność", "setapowiec"]},
     {name:"Oli", surname:"Wasiuk", badges: ["łączność", "setupowiec"]},
-    {name:"Dawid", surname:"Pawlak", badges: []},
+    {name:"Dawid", surname:"Pawlak", badges: ["łączność", "setupowiec"]},
     {name: "Hanna", surname: "Hryshkevich"},
 
 ]
