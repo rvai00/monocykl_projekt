d38628c081ad0f5a890fb31b5a911b236d18cf00
arwondianlu <120572220+arwondianlu@users.noreply.github.com>
2023-01-27 11:44:51 +0100

    edycja w app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import './App.css';
 import {users}  from "./data/users"
 import {Profile} from  "./components/molecules/Profile"
 
+function App() {
+
+  return (
+    <div className="App">
+      {users.map(user => <Profile user={user}/> )}
+
+    </div>
+  );
+}
+
+export default App;
+
+// const name = "Oli" 
+// const surname = "Wasiuk"
+// const name_hanna = "Hanna Hryshkevich"
+// const name_nina = "Nina" 
+// const surname_nina = "Niewińska"
+// const name_agata = "Agata"
+// const surname_agata = "Sawicka"
+// const agata = updateNameAgata(name_agata, surname_agata);
+// const dawid = generateDawid("Dawid Pawlak");
+// const oli = updateName(name, surname);
+// const nina = updateNameNina(name_nina, surname_nina)
+
+
 const UpdateNameHanna = (props) => {
   console.log(props)
   const updatedNameHanna = props.name.toUpperCase();
@@ -23,30 +48,3 @@ const updateName = (name, surname) => {
   const newSurname = surname.slice (4,5)
   return {name: newName,surname: newSurname};
 }
-
-
-function App() {
-
-  return (
-    <div className="App">
-      {users.map(user => <UpdateNameHanna name={user.name}/> )}
-      {users.map(user => <GenerateDawid user={user}/> )}
-      {users.map(user => <Profile user={user}/> )}
-
-    </div>
-  );
-}
-
-export default App;
-
-// const name = "Oli" 
-// const surname = "Wasiuk"
-// const name_hanna = "Hanna Hryshkevich"
-// const name_nina = "Nina" 
-// const surname_nina = "Niewińska"
-// const name_agata = "Agata"
-// const surname_agata = "Sawicka"
-// const agata = updateNameAgata(name_agata, surname_agata);
-// const dawid = generateDawid("Dawid Pawlak");
-// const oli = updateName(name, surname);
-// const nina = updateNameNina(name_nina, surname_nina)
--- src/data/users.js
+++ src/data/users.js
@@ -1,6 +1,6 @@
 export const users = [
     {name:"Nina", surname: "Niewińska"},
-    {name:"Agata", surname:"Sawicka"},
+    {name:"Agata", surname:"Sawicka", badges: ["łączność", "setapowiec"]},
     {name:"Oli", surname:"Wasiuk"},
     {name:"Dawid", surname:"Pawlak", badges: []},
     {name: "Hanna", surname: "Hryshkevich"},
