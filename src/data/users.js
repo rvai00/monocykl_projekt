2105e7f9cf07b7839a758a0491b120960905b6ad
arwondianlu <120572220+arwondianlu@users.noreply.github.com>
2023-02-03 11:32:00 +0100

    dodanie odznaki

--- src/data/users.js
+++ src/data/users.js
@@ -1,6 +1,6 @@
 export const users = [
     {name:"Nina", surname: "Niewińska"},
-    {name:"Agata", surname:"Sawicka", badges: ["łączność", "setapowiec"]},
+    {name:"Agata", surname:"Sawicka", badges: ["łączność", "setapowiec", "objektywizm"]},
     {name:"Oli", surname:"Wasiuk", badges: ["łączność", "setupowiec"]},
     {name:"Dawid", surname:"Pawlak", badges: ["łączność", "setupowiec"]},
     {name: "Hanna", surname: "Hryshkevich", badges: ["łączność", "setupowiec"]},
