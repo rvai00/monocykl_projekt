478b98ade512a1ece9c99f17641d1ca9da8113af
dawerty98 <100321042+dawerty98@users.noreply.github.com>
2023-02-03 12:11:03 +0100

    Wyglad profilu

--- src/components/molecules/Profile.jsx
+++ src/components/molecules/Profile.jsx
@@ -1,8 +1,24 @@
+import styled from "styled-components";
 import {Name} from  "../atoms/Name"
 import { Badges} from "../atoms/Badges"
 
 export const Profile = (props) => {
-    return <div>
+    return <StyledWrapper>
       <Name name={props.user.name}/> 
-    <Badges badges={props.user.badges}/></div>
+    <Badges badges={props.user.badges}/></StyledWrapper>
   }
+
+const StyledWrapper = styled("div")(
+()=>({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1ex solid black",
+    paddingInline: "16px",
+    paddingBlock: "20px",
+    width: "300px", //;_;
+    margin: "16x",
+    borderRadius: "15px 20px"
+})
+);
